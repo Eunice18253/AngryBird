6296cae37f49dd1866b530ec94d5881807cea754
Eunice18253 <eunicengzh@gmail.com>
2021-02-27 10:41:08 +0800

    added slingshot class

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg, platform;
-var log6, chain;
+var chain;
 
 function preload() {
 
@@ -38,16 +38,12 @@ function setup() {
     log5 = new Log(870, 120, 150, -PI / 7);
 
     bird = new Bird(100, 100);
-    log6 = new Log(230, 180, 80, PI / 2);
-    chain = new Chain(bird.body, log6.body);
+    chain = new Slingshot(bird.body, { x: 200, y: 50 });
 }
 
 function draw() {
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -65,6 +61,13 @@ function draw() {
 
     bird.display();
     platform.display();
-    log6.display();
     chain.display();
+}
+function mouseDragged() {
+    // bird.body.position.x = mouseX;
+    // bird.body.position.y = mouseY;
+    Matter.Body.setPosition(bird.body, { x: mouseX, y: mouseY })
+}
+function mouseReleased() {
+    chain.fly();
 }
